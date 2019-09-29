d843c386c4467097ba3735b610f4a033055bf464
Mike Cooper <mikescooper@icloud.com>
2019-09-29 03:11:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,22 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML( open(index_url))
+    student_cards_array = []
+    
+    doc.css("student-card").each_with_index do |card, i|
+     student_cards_array[i] = {
+      :name => card.css(".card-text-continer.student-name").text,
+      :location => card.css(".card-text-container.student-location").text,
+      :profile_url => card.css("a").attribute("href").value
+      }
+      binding.pry
+    end
+
     
   end
 
@@ -13,3 +26,6 @@ class Scraper
 
 end
 
+
+
+#doc.css(".card-text-container h4").map{|student| student.text}
