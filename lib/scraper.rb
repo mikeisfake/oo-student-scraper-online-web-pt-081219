5ca0f1ab36cafcfbb3545b7527a5eed8665425c7
Mike Cooper <mikescooper@icloud.com>
2019-09-29 04:04:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML( open(profile_url))
     
-    socials = doc.css(".social-icon-container")
+    socials = doc.css(".social-icon-container").children.css("a").map{|a| a.attribute("href")}
     
     quote = doc.css(".profile-quote").text
     bio = doc.css(".bio-content p").text
