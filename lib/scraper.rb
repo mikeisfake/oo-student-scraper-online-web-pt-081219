3026a308ca964695f9bb3b9bf6003261b81a2b4a
Mike Cooper <mikescooper@icloud.com>
2019-09-29 03:43:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML( open(profile_url))
     
-    bio = doc.css(".details-container.description-holder")
+    bio = doc.css(".details-container.description-holder").text
     
     student_profile = {
       :twitter => "",
@@ -33,7 +33,7 @@ class Scraper
       :profil_quote => "",
       :bio => ""
     }
-    
+    binding.pry
     
   end
 
